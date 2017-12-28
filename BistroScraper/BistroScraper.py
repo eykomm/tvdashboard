3387d245cc428d6ee4616747428d5cce482f1390
Lars <eykomm@users.noreply.github.com>
2017-12-28 20:45:48 +0100

    scrapy script adjustments

--- BistroScraper/BistroScraper.py
+++ BistroScraper/BistroScraper.py
@@ -1,4 +1,5 @@
 # small scrapy script to extract the current menu from MedienBistro
+# - by LBr 2017
 
 import scrapy
 
@@ -8,15 +9,17 @@ class MenuItem(scrapy.Item):
 	meal = scrapy.Field()
 	price = scrapy.Field()
 
+# Define class for website Spider
 class MenuSpider(scrapy.Spider):
 	name = 'menu'
 	start_urls = ['http://www.caribbean-embassy.de/mittagstisch']
 
+	# Extract table items from website - not working 100% yet
 	def parse(self, response):
 		rows = response.xpath('//table/tbody/tr')
 		for row in rows:
 			item = MenuItem()
 			item['day'] = row.xpath('th/text()').extract()
-			item['meal'] = row.xpath('td[1]/text()').extract()
+			item['meal'] = row.xpath('td/text()').extract()
 			item['price'] = row.xpath('td[2]/text()').extract()
 			yield item
